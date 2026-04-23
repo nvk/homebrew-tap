d7f95090b78207023167a646e01dd75cb3a03286
nvk <rodolfo@rnvk.org>
2026-04-22 20:22:22 -0600

    Bump ascii-banner to 0.3.1

--- Formula/ascii-banner.rb
+++ Formula/ascii-banner.rb
@@ -3,8 +3,8 @@ class AsciiBanner < Formula
 
   desc "Convert text to ASCII art banners using 328 FIGlet fonts"
   homepage "https://github.com/nvk/ascii-banner"
-  url "https://files.pythonhosted.org/packages/source/a/ascii-banner/ascii_banner-0.3.0.tar.gz"
-  sha256 "5518f6d70c85d89634ae6d3d3e0e3a6cba4376f709fef9485a1d9ccb246a6607"
+  url "https://files.pythonhosted.org/packages/source/a/ascii-banner/ascii_banner-0.3.1.tar.gz"
+  sha256 "a5734fd56fd0bba4c6245f488063a936a2555c7baea7d2c05a6e92858e002427"
   license "MIT"
 
   depends_on "python@3.12"
