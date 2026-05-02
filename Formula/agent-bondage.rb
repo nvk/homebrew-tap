c1f040648a92d119f11f5e21115a801e6bd89119
nvk <git@nvk.org>
2026-05-01 20:12:10 -0600

    agent-bondage 0.2.3

--- Formula/agent-bondage.rb
+++ Formula/agent-bondage.rb
@@ -2,9 +2,9 @@ class AgentBondage < Formula
   desc "Trusted C launcher for local AI agent stacks"
   homepage "https://github.com/nvk/bondage"
   url "https://github.com/nvk/bondage.git",
-      tag:      "v0.2.2",
-      revision: "e6b8fc54b155ba01c177445c723acc41ac3b51f0"
-  version "0.2.2"
+      tag:      "v0.2.3",
+      revision: "8aa706f94e897103aba96642443f742b7d1eaaf2"
+  version "0.2.3"
   head "https://github.com/nvk/bondage.git", branch: "main"
 
   def install
