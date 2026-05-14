4a24be59780c7b126a2c338a558e1fef95726437
nvk <git@nvk.org>
2026-05-14 06:12:59 -0600

    Update agentnoise to 0.1.5

--- Formula/agentnoise.rb
+++ Formula/agentnoise.rb
@@ -2,8 +2,8 @@ class Agentnoise < Formula
   desc "Chat with local coding agents through White Noise"
   homepage "https://agentnoise.com"
   url "https://github.com/nvk/agentnoise.git",
-      tag:      "v0.1.4",
-      revision: "8353331c58dd978f23e4140e59cb79b8ec9b07b0"
+      tag:      "v0.1.5",
+      revision: "c23a00f1f0bcda7d49f37ecc1a4dbcf3be79a52f"
   license "MIT"
   head "https://github.com/nvk/agentnoise.git", branch: "main"
 
@@ -35,6 +35,6 @@ class Agentnoise < Formula
   end
 
   test do
-    assert_match "agentnoise 0.1.4", shell_output("#{bin}/agentnoise --version")
+    assert_match "agentnoise 0.1.5", shell_output("#{bin}/agentnoise --version")
   end
 end
